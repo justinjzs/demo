40f1377eda27ab31d4a137b22c14a598ce7a1917
justinjin <Justin.Jin@grapecity.com>
2016-12-01 12:30:40 +0800

    for learning node.js

---
+++ node-learning/streaming.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+
+// fs.readFile('./test.json', function (er, data) { //filr reading
+//     console.log(data.toString());
+// });
+
+// var stream = fs.createReadStream('./test.json');  //streaming 
+// stream.on('data', function (chunk) {
+//     console.log(chunk.toString());
+// });
+// stream.on('end', function () {
+//     console.log('finished');
+// });
+
+var http = require('http');                         //readfiles pipe to res 
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    fs.createReadStream('./test.json').pipe(res);
+}).listen(2016);
+console.log('Server running on http://localhost:2016/');
